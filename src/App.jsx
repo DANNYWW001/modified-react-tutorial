2f6d81c669dc07493d4a3973e2f875616acfe5ae
DANNYWW001 <lekandaniel005@gmail.com>
2025-04-09 10:35:58 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Contact from "./components/Contact";
+import Product from "./components/Product";
+import Footer from "./components/Footer";
+import Dynamic from "./components/Dynamic";
+import Props from "./components/Props";
+import JumiaClone from "./components/JumiaClone";
+import SimpleList from "./components/SimpleList";
+import ComplexList from "./components/ComplexList";
+import Mystore from "./components/Mystore";
+import LastViewed from "./components/LastViewed";
+
+function App() {
+  return (
+    <>
+      {/* <Navbar />
+      <Hero />
+      <Product />
+      <Contact />
+      <Footer /> */}
+      {/* <Dynamic /> */}
+      {/* <Props /> */}
+      {/* <JumiaClone/> */}
+      {/* <SimpleList /> */}
+      {/* <ComplexList /> */}
+      <Mystore />
+      <LastViewed />
+    </>
+  );
+}
+
+export default App;
